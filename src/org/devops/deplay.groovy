19f5404a28d1e85fd6b130273454219bde674add
hu417 <70016878+hu417@users.noreply.github.com>
2021-03-28 21:09:14 +0800

    Update deplay.groovy

--- src/org/devops/deplay.groovy
+++ src/org/devops/deplay.groovy
@@ -1,7 +1,11 @@
 package org.devops
 
 //saltstack
-
 def SaltDeplay(host,func){
 		sh " salt \"${host}\" \"${func}\" "
+}
+
+// ansible
+def Ansible(host,func){
+		sh " ansible \"${host}\" \"${func}\" "
 }
